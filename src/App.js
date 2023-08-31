1564d9b6eb8c0b982077aa3aeede04d963acda88
Bibek <bibek9818917036@gmail.com>
2023-09-01 00:16:29 +1000

    work on UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import IceCreamView from "./features/icecream/IceCreamView";
 import CakeView from "./features/cake/CakeView";
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <CakeView />
       <IceCreamView />
-    </>
+    </div>
   );
 }
 
